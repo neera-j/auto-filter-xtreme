98e59ac12dc8ea88f4e02cd3de9b14e40a3d522b
neera-j <86142113+neera-j@users.noreply.github.com>
2021-09-22 21:37:24 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,7 +7,7 @@ class Translation(object):
     from bot import BO 
     
     
-    START_TEXT = "<b>Hey {} my name is !!</b>" (BOT_NAME)
+    START_TEXT = "<b>Hey {} my name is !!</b>"
 "<>Am Just A Advance Auto Filter Bot Created by admins of <b>MOVIE DEPARTED</b>...For their group. "
 "I can't be added to other Groups so Don't Waste your time here"
 
